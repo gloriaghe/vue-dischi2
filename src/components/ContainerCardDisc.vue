57c864de2889f1c4a01c6d5d6e365f8291653d95
Gloria <gloghe@yahoo.it>
2022-06-13 12:52:42 +0200

    genre

--- src/components/ContainerCardDisc.vue
+++ src/components/ContainerCardDisc.vue
@@ -1,26 +1,35 @@
 <template>
   <div v-if="loadingCard" id="loadCard">Loading....</div>
 
-  <section id="discContainer" v-else>
-    <CardMusic v-for="(element, i) in musicCard" :key="i" :singleCard="element" />
-  </section>
+  <div v-else>
+    <SearchMusic @search="searchGenere"/>
+      
+    <section id="discContainer">
+
+      <CardMusic v-for="(element, i) in filtredmusicCard" :key="i" :singleCard="element" />
+    </section>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 import CardMusic from './CardMusic.vue';
+import SearchMusic from './SearchMusic.vue';
+
 
 export default {
   name: "ContainerCardDisc",
   components: {
-    CardMusic
+    CardMusic,
+    SearchMusic
   },
   data() {
     return {
       apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
       musicCardTotal: {},
       musicCard: {},
-      loadingCard: true
+      loadingCard: true,
+      userGenre:""
     }
   },
   created() {
@@ -41,13 +50,30 @@ export default {
         })
     },
 
+    searchGenere(genreuser){
+      this.userGenre = genreuser;
+      console.log(genreuser)
+    }
+  },
+  computed: {
+    filtredmusicCard(){
+      if(this.userGenre === ""){
+        return this.musicCard;
+      } else{
+        return this.musicCard.filter(item =>{
+          return item.genre.toLowerCase().includes(this.userGenre.toLowerCase());
+        })
+      }
+
+
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-#loadCard{
+#loadCard {
   font-size: 30px;
   text-align: center;
   margin-top: 20px;
---
+++ src/components/SearchMusic.vue
@@ -0,0 +1,41 @@
+<template>
+  
+    <div id="menuGenere">
+      <select name="Genere" v-model="inputGenre" @input="$emit('search', inputGenre)">
+        <option disabled value="">Please select genre:</option>
+        <option value="ROCK">ROCK</option>
+        <option value="POP">POP</option>
+        <option value="JAZZ">JAZZ</option>
+        <option value="METAL">METAL</option>
+      </select>
+    </div>
+   
+</template>
+
+<script>
+
+
+export default {
+  name: "SearchMusic",
+  data() {
+    return {
+      inputGenre: "",
+    }
+  },
+  
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+
+#menuGenere{
+  display: flex;
+  justify-content: center;
+  margin: 10px;
+}
+
+
+</style>
